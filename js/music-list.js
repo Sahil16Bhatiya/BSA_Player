b7e76709c6e8e76e0581f891eb15881081858e77
Sahil16Bhatiya <92774669+Sahil16Bhatiya@users.noreply.github.com>
2023-03-05 23:31:13 -0800

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,174 @@
+/*
+ Note :
+ images must be in a .jpg format and audio must be in .mp3 format
+*/
+
+let allMusic = [
+  {
+    name: "Master -2020",
+    artist: "Vijay",
+    img: "m1",
+    src: "m1"
+  },
+  {
+    name: "Such a whore",
+    artist: "Holiwood Song",
+    img: "m2",
+    src: "m2"
+  },
+  {
+    name: "Surviva",
+    artist: "South Ajeet",
+    img: "m3",
+    src: "m3"
+  },
+  {
+    name: "Varisu -Thee Thalapathy",
+    artist: "Vijay",
+    img: "m4",
+    src: "m4"
+  },
+  {
+    name: "Unstoppable",
+    artist: "Holiwood Song",
+    img: "m5",
+    src: "m5"
+  },
+  {
+    name: "Choix de vie remix",
+    artist: "Holiwood Song",
+    img: "m6",
+    src: "m6"
+  },
+  {
+    name: "DJ -Seeti Maar",
+    artist: "Allu Arjun",
+    img: "m7",
+    src: "m7"
+  },
+  {
+    name: "Saaho Bgm",
+    artist: "Prabhash",
+    img: "m8",
+    src: "m8"
+  },
+  {
+    name: "KGF 2",
+    artist: "Rocky -Yash",
+    img: "m9",
+    src: "m9"
+  },
+  {
+    name: "Samajavaragamana",
+    artist: "Allu Arjun",
+    img: "m10",
+    src: "m10"
+  },
+
+  {
+    name:"Rolex",
+    artist:"Surya",
+    img: "m11",
+    src: "m11"
+  },
+  {
+   name:"Mann Mera - Lofi ",
+   artist:"hindi song",
+    img: "m12",
+    src: "m12"
+  },
+  {
+    name:"Bheeshma",
+    artist:"Nithiin",
+    img: "m13",
+    src: "m13"
+  },
+  {
+    name:"taqdeer",
+    artist:"violin ringtone",
+    img: "m14",
+    src: "m14"
+  },
+  {
+     name:"KGF2",
+     artist:"MOTHER ",
+    img: "m15",
+    src: "m15"
+  },
+  {
+    name:"Super Khiladi 3",
+    artist:"ram pothineni",
+    img: "m16",
+    src: "m16"
+  },
+  {
+    name:"ALa Vaikunthapuraamulllo",
+    artist:"Allu Arjun",
+    img: "m17",
+    src: "m17"
+  },
+  {
+    name:"Maari",
+    artist:"Dhanush",
+    img: "m18",
+    src: "m18"
+  },
+  {
+    name:"Vikram",
+    artist:"Kamal Hasan",
+    img: "m19",
+    src: "m19"
+  },
+  {
+    name:"Theri",
+    artist:"Samantha",
+    img: "m20",
+    src: "m20"
+  },
+  {
+    name:"GeetaGovinda",
+    artist:"Rashmika",
+     img: "m21",
+    src: "m21"
+  },
+  {
+    name:"Super Khiladi 4",
+    artist:"Kirthi Suresh",
+    img: "m22",
+    src: "m22"
+  },
+  {
+    name:"Puspa",
+    artist:"Allu Arjun",
+    img: "m23",
+    src: "m23"
+  },
+  {
+    name:"DJ BGM",
+    artist:"Allu Arjun",
+    img: "m25",
+    src: "m25"
+  },
+  {
+    name:"Beast",
+    artist:"Vijay",
+    img: "m24",
+    src: "m24"
+  },
+  {
+    name:"Vikram",
+    artist:"vijay sethupathi",
+    img: "m26",
+    src: "m26"
+  }
+
+
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
